a1a25c9a6123008a4ed73ebd3dc2bd8808a8d62e
TorinDuboShit <spelyovin196@gmail.com>
2021-03-03 17:28:24 +0300

    Добавьте файлы проекта.

---
+++ Lb1s4/MyForm.cpp
@@ -0,0 +1,53 @@
+#include "MyForm.h"
+#include <string>
+#include <cstdlib>
+#include <Windows.h>
+using namespace Lb1s4; // Название проекта
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	return 0;
+}
+
+System::Void Lb1s4::MyForm::button1_Click(System::Object^ sender, System::EventArgs^ e){
+	double x, y;
+	double pi = 3.141592653589793;
+	boolean key = true;
+
+	String^ t = textBox1->Text;
+
+	for (int i = 0; i < (t->Length); i++) {
+		if (t[i] == '0' || t[i] == '1' || t[i] == '2' \
+			|| t[i] == '3' || t[i] == '4' || t[i] == '5' \
+			|| t[i] == '6' || t[i] == '7' || t[i] == '8' \
+			|| t[i] == '9' || t[i] == '-' || t[i] == ',');
+		else {
+			key = false;
+		}
+	}
+
+	if (key == false) {
+		textBox2->Text = System::Convert::ToString("Ошибка ввода! Повторите!");
+	}
+	else {
+		x = System::Convert::ToDouble(textBox1->Text);
+		y = (sin(sin(x) + exp(cos(x)) + pow(x, 2))) * (pow((sin(pi * pow(x, 2))) + log(pow(x, 2)), 0.25));
+		textBox2->Text = System::Convert::ToString(y);
+
+		if ((x < 1) && (x != 0)) {
+			textBox2->Text = System::Convert::ToString("Ошибка ввода! Повторите!(x>1)");
+		}
+		else if (x == 0)
+		{
+			textBox2->Text = System::Convert::ToString("Функция не пределена");
+		}
+	}
+}
+
+System::Void Lb1s4::MyForm::button2_Click(System::Object^ sender, System::EventArgs^ e){
+	textBox1->Text = L"";
+	textBox2->Text = L"";
+}
---
+++ Lb1s4/MyForm.h
@@ -0,0 +1,170 @@
+#pragma once
+#include <math.h>
+
+namespace Lb1s4 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Button^ button2;
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(35, 9);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(315, 33);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(12, 47);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(84, 32);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"   Введите \r\nпараметр х:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(102, 57);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(96, 22);
+			this->textBox1->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(269, 48);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(102, 40);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Вычислить значение у";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(12, 97);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(137, 48);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Значение функции \n\r\nу=";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(35, 123);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(228, 22);
+			this->textBox2->TabIndex = 5;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(269, 105);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(102, 40);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"Очистить";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ControlLightLight;
+			this->ClientSize = System::Drawing::Size(383, 154);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pictureBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->Name = L"MyForm";
+			this->Text = L"Решение функции";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e);
+};
+}
